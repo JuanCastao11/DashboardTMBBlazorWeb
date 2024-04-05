b4e1d063373640da33642b096350543c873a6bc0
Juan Manuel Castaño <juanmanuelcastano11b@gmail.com>
2024-04-05 16:45:51 -0500

    login

--- Prueba/obj/Debug/net8.0/Prueba.AssemblyInfo.cs
+++ Prueba/obj/Debug/net8.0/Prueba.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69fedd099e01fc37db93df1cfd3a17ada20b43c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4d6665c0858d58ff88ddce582b887fd80fbcdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
